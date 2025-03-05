48dd84b85636f2c38695fa0fc4e95e245c16d1f8
YuSawan <sawada.yuya.sr7@is.naist.jp>
2025-03-06 05:02:41 +0900

    modify shell

--- shell/finetune/finetune_meta.sh
+++ shell/finetune/finetune_meta.sh
@@ -12,20 +12,25 @@ train_file=~/data/ner/${dataset}/train.jsonl
 val_file=~/data/ner/${dataset}/validation.jsonl
 test_file=~/data/ner/${dataset}/test.jsonl
 output_dir=save_models/${dataset}/${model_name}
+format='single' # single or multi
+extend_context=True
+language='en' # en or ja
 seed=0
 
-
-# uv run torchrun --nproc_per_node 1 src/train.py
-uv run python src/train.py \
+mkdir -p $output_dir
+uv run torchrun --nproc_per_node 1 src/train.py
     --do_train \
     --do_eval \
-    --model_name $model_name \
+    --model $model_name \
     --train_file $train_file \
     --validation_file $val_file \
-    --eval_file $eval_file \
+    --test_file $test_file \
+    --format $format \
+    --language $language \
+    --extend_context $extend_context \
     --output_dir $output_dir \
     --per_device_train_batch_size 2 \
-    --per_device_eval_batch_size 8 \
+    --per_device_eval_batch_size 4 \
     --run_name ${model_name}_${seed} \
     --seed $seed
 
