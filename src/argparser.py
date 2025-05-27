c5eec50fcd755e5dedd58b8e5158871d40dc2924
YuSawan <sawada.yuya.sr7@is.naist.jp>
2025-05-27 22:01:00 +0900

    modify format

--- src/argparser.py
+++ src/argparser.py
@@ -31,6 +31,20 @@ class ModelArguments:
     prev_path: Optional[str] = None
     checkpoint_path: Optional[str] = None
 
+@dataclass
+class GptModelArguments:
+    """Model arguments for GPT."""
+    model_name: str
+    total_cost_limit: float
+    top_p: float
+    temperature: float
+    seed: int
+    n: int
+    max_token_length: int
+    mode: str # "generate", "estimate", "debug", or "batch"
+    output_dir: str
+    cache_dir: Optional[str] = None
+
 
 def parse_args() -> tuple[DatasetArguments, ModelArguments, TrainingArguments]:
     parser = ArgumentParser()
@@ -51,3 +65,34 @@ def parse_args() -> tuple[DatasetArguments, ModelArguments, TrainingArguments]:
     training_args = replace(training_args, **config)
 
     return arguments, model_args, training_args
+
+
+def parse_args_gpt() -> tuple[DatasetArguments, GptModelArguments]:
+    parser = ArgumentParser()
+    parser.add_argument(
+        "--config_file", "-c", metavar="FILE", required=True,
+        help="Path to the configuration file in YAML format."
+    )
+    parser.add_argument(
+        "--output_dir", "-o", metavar="DIR", required=True,
+        help="Directory to save the output files."
+    )
+    parser.add_argument(
+        "--mode", "-m", metavar="MODE", default="estimate",
+        choices=["generate", "estimate", "debug", "batch"],
+        help="Mode of operation: 'generate' for generating outputs, 'estimate' for cost estimation, 'debug' for debugging, or 'batch' for batch processing."
+    )
+
+    args, _ = parser.parse_known_args()
+    config = vars(load_config_as_namespace(args.config_file))
+
+    data_config = config.pop("dataset")
+    model_config = config.pop("gpt_model")
+
+    model_config["output_dir"] = args.output_dir
+    model_config["mode"] = args.mode
+
+    arguments = DatasetArguments(**data_config)
+    model_args = GptModelArguments(**model_config)
+
+    return arguments, model_args
