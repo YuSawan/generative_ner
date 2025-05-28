419a74231436f3cbf5845697874872e821fe4fce
YuSawan <sawada.yuya.sr7@is.naist.jp>
2025-05-28 13:21:40 +0900

    add predictions

--- src/prediction/pred.py
+++ src/prediction/pred.py
@@ -1,42 +1,76 @@
 from typing import Any
 
 import torch
-import wandb
 from datasets import Dataset
 from peft import PeftModelForCausalLM
 from tqdm.auto import tqdm
 
-from data.preprocessor import Preprocessor
+import wandb
+from src.data.preprocessor import Preprocessor
+from src.gpt.base.utils import regex
 
+
+def _generate(messages: list[dict[str, Any]], model: PeftModelForCausalLM, preprocessor: Preprocessor) -> str:
+    model_input = messages[:-1]
+    tokenized_chat = preprocessor.tokenizer.apply_chat_template(
+        model_input,
+        tokenize=True,
+        add_generation_prompt=True,
+        return_tensors="pt",
+    )
+    tokenized_chat = tokenized_chat.to(model.device)
+    generated_tokens = model.generate(tokenized_chat)
+    generated_text = preprocessor.tokenizer.decode(generated_tokens[0]).replace(preprocessor.tokenizer.eos_token, "\n")
+    generated_text = generated_text.split("<|start_header_id|>assistant<|end_header_id|>")[-1].strip()
+    return generated_text
 
 @torch.no_grad()
 def predict(
         model: PeftModelForCausalLM,
         predict_dataset: Dataset,
-        preprocessor: Preprocessor
+        preprocessor: Preprocessor,
+        names2labels: dict[str, str],
     ) -> list[dict[str, Any]]:
-    pbar = tqdm(total=len(predict_dataset), desc="Eval")
-    results = []
+    format = preprocessor.format
+    pbar = tqdm(total=len(predict_dataset), desc="Predict")
+    predictions = []
     for document in predict_dataset:
         pbar.update(1)
-        for messages in preprocessor.get_messages(document):
-            model_input = messages[:-1], messages[-1]
-            gold_output = messages[-1]['content']
-            tokenized_chat = preprocessor.tokenizer.apply_chat_template(
-                model_input,
-                tokenize = True,
-                add_generation_prompt=True,
-                return_tensors="pt",
-            )
-            tokenized_chat = tokenized_chat.to(model.device)
-            generated_tokens = model.generate(tokenized_chat, max_new_tokens=512)
-            generated_text = preprocessor.tokenizer.decode(generated_tokens[0]).replace(preprocessor.tokenizer.eos_token, "\n")
-            generated_text = generated_text.split("<|start_header_id|>assistant<|end_header_id|>")[-1].strip()
-            golds = preprocessor.parse_output(gold_output)
-            preds = preprocessor.parse_output(generated_text)
-            results.append({"id": document["id"], "text": model_input[1]["content"], "golds": golds, "preds": preds, 'generated_text': generated_text})
+        for example in document["examples"]:
+            eid = example["id"]
+            text = example["text"]
+            if format in ['collective', 'universal']:
+                gold_spans = [(ent["start"], ent["end"], ent["label"]) for ent in example["entities"]]
+                for messages in preprocessor.get_messages([example]):
+                    generated_text = _generate(messages, model, preprocessor)
+                    pred_spans = []
+                    preds = preprocessor.parse_output(generated_text, format)
+                    for p in sorted(set(preds)):
+                        if ": " not in p:
+                            continue
+                        label, mention = p.split(": ")
+                        try:
+                            pred_spans.extend([(s, e, names2labels[label]) for s, e in regex(text.lower(), mention)])
+                        except KeyError:
+                            pred_spans.extend([(s, e, label) for s, e in regex(text.lower(), mention)])
+                    predictions.append({"id": eid, "text": text, "golds": gold_spans, "preds": pred_spans, 'generated_text': generated_text})
 
-    return results
+            elif format == 'individual':
+                for label, messages in zip(names2labels.values(), preprocessor.get_messages([example])):
+                    gold_spans = [(ent["start"], ent["end"], ent["label"]) for ent in example["entities"] if ent["label"] == label]
+                    generated_text = _generate(messages, model, preprocessor)
+                    pred_spans = []
+                    preds = preprocessor.parse_output(generated_text, format)
+                    for p in sorted(set(preds)):
+                        try:
+                            pred_spans.extend([(s, e, names2labels[label]) for s, e in regex(text.lower(), p)])
+                        except KeyError:
+                            pred_spans.extend([(s, e, label) for s, e in regex(text.lower(), p)])
+                    predictions.append({"id": eid, "text": text, "golds": gold_spans, "preds": pred_spans, 'generated_text': generated_text})
+            else:
+                raise NotImplementedError(f"Format {format} is not implemented")
+
+    return predictions
 
 
 def submit_wandb_predict(predictions: list[dict[str, Any]]) -> None:
@@ -44,11 +78,14 @@ def submit_wandb_predict(predictions: list[dict[str, Any]]) -> None:
     result_table = wandb.Table(columns=columns)
 
     for prediction in predictions:
+        golds = sorted(set([f"{label}: {prediction['text'][s:e]}" for s, e, label in prediction["golds"]]))
+        preds = sorted(set([f"{label}: {prediction['text'][s:e]}" for s, e, label in prediction["preds"]]))
+
         result_table.add_data(
             prediction["id"],
             prediction["text"],
-            ', '.join(prediction["golds"]),
-            ', '.join(prediction["preds"]),
+            ', '.join(golds),
+            ', '.join(preds),
             prediction["generated_text"]
         )
     wandb.log({"predictions": result_table})
